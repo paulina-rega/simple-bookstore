1ef70334fd332543b5549818c40f648f58d699a0
Paulina <rega.paulina@icloud.com>
2020-02-02 19:11:09 +0100

    tiny change

--- index.php
+++ index.php
@@ -14,8 +14,10 @@ init_cart();
 <html>
 <head>
 <title> Czytelnia </title>
-<meta http-equiv="Content-type" content="text/html"; charset="utf-8">
-<meta name="Czytelnia" content=" [wstaw tu opis strony] "> <meta name="Keywords" content=" [wstaw tu slowa kluczowe] "> <meta name="Author" content=" [dane autora] ">
+<meta http-equiv="Content-type" content="text/html">
+<meta charset="utf-8">
+<meta name="Czytelnia" content=" [wstaw tu opis strony] ">
+<meta name="Keywords" content=" [wstaw tu slowa kluczowe] "> <meta name="Author" content=" [dane autora] ">
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
